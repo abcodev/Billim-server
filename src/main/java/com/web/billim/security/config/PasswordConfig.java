f47759a613be93c65915d288ef735af023f2a2cf
Jang <ahhhaaah@gmail.com>
2023-08-02 13:50:32 +0900

    fix: PasswordConfig 설정

--- src/main/java/com/web/billim/security/config/PasswordConfig.java
+++ src/main/java/com/web/billim/security/config/PasswordConfig.java
@@ -1,6 +1,13 @@
 package com.web.billim.security.config;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class PasswordConfig {
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
