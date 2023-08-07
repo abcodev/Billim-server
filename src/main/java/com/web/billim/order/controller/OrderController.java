aa2b402cbabb9aa1db761a4112e0a7976ae22ae5
Jang <ahhhaaah@gmail.com>
2023-08-07 20:31:44 +0900

    feat: 채팅 읽음처리, 리뷰 작성시 포인트 지급 fix: 취소시 쿠폰 사용 안했을 경우 취소된 예약 정보 분리, 상품 디테일 수정

--- src/main/java/com/web/billim/order/controller/OrderController.java
+++ src/main/java/com/web/billim/order/controller/OrderController.java
@@ -44,7 +44,7 @@ public class OrderController {
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "*마이페이지 상품 구매 목록 조회", description = "마이페이지에서 구매 목록을 조회한다.")
+    @Operation(summary = "마이페이지 상품 구매 목록 조회", description = "마이페이지에서 구매 목록을 조회한다.")
     @GetMapping("/my/purchase")
     public ResponseEntity<MyOrderListResponse> myOrder(@AuthenticationPrincipal long memberId) {
         return ResponseEntity.ok(orderService.findMyOrder(memberId));
@@ -56,7 +56,7 @@ public class OrderController {
         return ResponseEntity.ok(productService.findMySalesList(memberId));
     }
 
-    @Operation(summary = "*마이페이지 판매 상품 상세정보", description = "판매중인 상품 클릭시 판매 주문 내역을 조회한다.")
+    @Operation(summary = "마이페이지 판매 상품 상세정보", description = "판매중인 상품 클릭시 판매 주문 내역을 조회한다.")
     @GetMapping("/my/sales/{productId}")
     public ResponseEntity<MySalesDetailResponse> mySalesDetail(@AuthenticationPrincipal long memberId, @PathVariable("productId") long productId) {
         return ResponseEntity.ok(orderService.findMySalesDetail(memberId, productId));
