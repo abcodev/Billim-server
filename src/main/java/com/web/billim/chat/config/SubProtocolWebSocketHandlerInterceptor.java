e9879c0eac6ad3ef7effaee14c0121fc3dbb9c77
Jang <ahhhaaah@gmail.com>
2023-08-23 14:13:12 +0900

    fix: 웹소켓 용량 설정

--- src/main/java/com/web/billim/chat/config/SubProtocolWebSocketHandlerInterceptor.java
+++ src/main/java/com/web/billim/chat/config/SubProtocolWebSocketHandlerInterceptor.java
@@ -9,7 +9,7 @@ public class SubProtocolWebSocketHandlerInterceptor extends DelegatingIntroducti
     protected Object doProceed(MethodInvocation mi) throws Throwable {
         if(mi.getMethod().getName().equals("afterConnectionEstablished") ) {
             WebSocketSession session = (WebSocketSession) mi.getArguments()[0];
-            session.setTextMessageSizeLimit(500*1024*1024);
+            session.setTextMessageSizeLimit(200*1024*1024);
         }
         return super.doProceed(mi);
     }
