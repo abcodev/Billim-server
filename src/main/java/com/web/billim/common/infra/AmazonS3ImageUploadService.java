4e3718d14e5110e9d5bd27848ae3fc461b49ec35
hyunjeong <ahhhaaah@gmail.com>
2023-11-14 09:57:12 +0900

    fix: email 스타일 수정

--- src/main/java/com/web/billim/common/infra/AmazonS3ImageUploadService.java
+++ src/main/java/com/web/billim/common/infra/AmazonS3ImageUploadService.java
@@ -50,10 +50,10 @@ public class AmazonS3ImageUploadService implements ImageUploadService {
 		}
 	}
 
+	// Base64
 	@Override
 	public String upload(String encodedImage, String path) {
 		// data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAASABIAAD/7QAsUGhvdG9zaG9wIDMuMAA4QklNBCUA .....
-		// Base64 로 인코딩된 이 이미지를 File 객체로 만들어 내야한다.
 		String fileName = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) + "_" + UUID.randomUUID();
 		File convertFile = ImageFileConvertHelper.convertBase64EncodedStringToImageFile(encodedImage, fileName);
 
@@ -84,18 +84,3 @@ public class AmazonS3ImageUploadService implements ImageUploadService {
 	}
 
 }
-
-/*
-    AWS S3 저장소에 Image 를 저장
-     1. AWS S3 에 Image 를 업로드 -> S3 는 정적 파일(Image, JS, CSS, HTML ..)을 저장하는 웹하드
-     2. AWS S3 에서 업로드된 파일에 접근할 수 있는 URL 을 제공해 줌
-     3. DB 에 이 URL 만 저장해두고
-     4. 그 URL 을 통해서 Client(사용자) 가 이미지에 접근할 수 있도록 한다.
-
-    AWS S3 세팅
-     1. AWS IAM 계정
-     2. 그 계정에 S3 Full Access 권한 부여
-     3. S3 Bucket 만들고
-     4. S3 Bucket 에 접근할 수 있는 권한, 정책
-     5. Spring 으로 넘어올거
- */
