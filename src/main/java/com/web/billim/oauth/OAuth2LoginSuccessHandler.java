57caaee964366196d8973ebae06774f36be135fe
hyunjeong <ahhhaaah@gmail.com>
2023-12-04 10:53:29 +0900

    fix: 카카오 queryParam

--- src/main/java/com/web/billim/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/web/billim/oauth/OAuth2LoginSuccessHandler.java
@@ -13,11 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
@@ -44,5 +46,14 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         String body = new ObjectMapper().writeValueAsString(loginResponse);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getWriter().write(body);
+
+        response.sendRedirect(UriComponentsBuilder.fromUriString("http://localhost:3000/login/callback")
+                .queryParam("accessToken", accessToken)
+                .queryParam("refreshToken", refreshToken)
+                .queryParam("memberId", memberId)
+                .build()
+                .encode(StandardCharsets.UTF_8)
+                .toUriString());
+
     }
 }
