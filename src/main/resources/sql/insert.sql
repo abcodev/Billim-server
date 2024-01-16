4e9fc83221d7f41e0f93948a7a8d8e5ed2e69fa9
hyunjeong <ahhhaaah@gmail.com>
2024-01-16 14:56:11 +0900

    release 240116

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -4,7 +4,7 @@ insert into product_category values (3, 'sporting');
 insert into product_category values (4, 'electronic');
 
 insert into coupon values (default, '회원가입 쿠폰', 10, 60, default, default);
-insert into coupon values (default, '그냥 주는 쿠폰', 30, 60, default, default);
+insert into coupon values (default, '그냥 주는 쿠폰', 5, 60, default, default);
 insert into coupon values (default, 'BRONZE 쿠폰', 5, 30, default, default);
 insert into coupon values (default, 'SILVER 쿠폰', 7, 30, default, default);
 insert into coupon values (default, 'GOLD 쿠폰', 10, 30, default, default);
